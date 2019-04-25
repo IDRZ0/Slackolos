851bf92d01a5e287beaa3483e1a604641623ad17
Justina-Mamani <justina.mamani.472@gmail.com>
2019-04-25 08:42:05 -0400

    toolbar, main

--- app/src/main/java/com/slackolos/kaumamusic/MainActivity.java
+++ app/src/main/java/com/slackolos/kaumamusic/MainActivity.java
@@ -1,15 +1,19 @@
 package com.slackolos.kaumamusic;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     private ActionBarDrawerToggle drawerToggle;
     private DrawerLayout drawerLayout;
 
@@ -17,20 +21,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.drawer_open,R.string.drawer_close);
-        drawerToggle.setDrawerIndicatorEnabled(false);
-        drawerToggle.setHomeAsUpIndicator(R.drawable.icon_hamburger);
-        drawerToggle.setToolbarNavigationClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                drawerLayout.openDrawer(GravityCompat.START);
-            }
-        });
         initViews();
+
+        if (this.toolbar != null) {
+
+            this.toolbar.setTitle(R.string.app_name);
+            this.toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.colorAccent));
+
+            drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+            drawerToggle.setDrawerIndicatorEnabled(false);
+            drawerToggle.setHomeAsUpIndicator(R.drawable.icon_hamburger);
+            drawerToggle.setToolbarNavigationClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            });
+
+        }
+
+
     }
 
-    public void initViews(){
+    public void initViews() {
+        this.toolbar = findViewById(R.id.toolbar);
         this.drawerLayout = findViewById(R.id.navigationDrawer);
     }
 }
