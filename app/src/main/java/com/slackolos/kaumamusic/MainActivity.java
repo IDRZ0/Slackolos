478318a6dfde5837ad000638a9da952a45c8009c
Ignacio del Río <nachitotigredelriozenteno@gmail.com>
2019-04-25 19:41:25 -0400

    register, drawer, main

--- app/src/main/java/com/slackolos/kaumamusic/MainActivity.java
+++ app/src/main/java/com/slackolos/kaumamusic/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
--- app/src/main/java/com/slackolos/kaumamusic/RegisterActivity.java
+++ app/src/main/java/com/slackolos/kaumamusic/RegisterActivity.java
@@ -1,6 +1,38 @@
 package com.slackolos.kaumamusic;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Context mContext;
+
+    private LinearLayout parent;
+
+    private TextView tUser;
+    private EditText user;
+
+    private TextView tPass;
+    private EditText pass;
+
+    private Button registrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        mContext = this;
+
+        parent = new LinearLayout(mContext);
+        parent.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT)
+        );
+
+    }
 }
