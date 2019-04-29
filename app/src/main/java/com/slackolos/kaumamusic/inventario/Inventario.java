c26f5d8c529f5ac68b6314c0cade8ef6dd9698ea
Ignacio del Río <nachitotigredelriozenteno@gmail.com>
2019-04-29 07:51:00 -0400

    inventario2

--- app/src/main/java/com/slackolos/kaumamusic/inventario/Inventario.java
+++ app/src/main/java/com/slackolos/kaumamusic/inventario/Inventario.java
@@ -10,9 +10,9 @@ public class Inventario {
 
     public static List<Cancion> getCanciones() {
         List<Cancion> canciones = new ArrayList<>();
-        canciones.add(new Cancion(1,"Otro Trago","Sech", R.drawable.cover));
-        canciones.add(new Cancion(2,"Si Se Da","Myke Towers", R.drawable.cover));
-        canciones.add(new Cancion(3,"Que Le De","Rauw Alejandro", R.drawable.cover));
+        canciones.add(new Cancion(1,"Otro Trago","Sech", R.drawable.otro));
+        canciones.add(new Cancion(2,"Si Se Da","Myke Towers", R.drawable.sise));
+        canciones.add(new Cancion(3,"Que Le De","Rauw Alejandro", R.drawable.quelede));
         return canciones;
     }
 }
